28bea52e5bd51dec7f23cb852cb3a6072fe4552d
Californ1a <rk_california@yahoo.com>
2018-08-28 05:52:44 -0400

    fix having multiple of the same map in playlist

--- src/js/createLevelList.js
+++ src/js/createLevelList.js
@@ -2,6 +2,12 @@ const rq = require("electron-require");
 const handleLi = rq("./js/handleLi.js");
 const saveXML = rq("./js/save.js");
 
+const padd = (n, width, z) => {
+	z = z || "0";
+	n = n.toString();
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+};
+
 const createLevelList = (ln) => {
 	const pad = document.getElementById("pad");
 	const drop = document.getElementById("drop");
@@ -13,11 +19,12 @@ const createLevelList = (ln) => {
 	drop.style.border = "1px solid white";
 	drop.style.width = "300px";
 	drop.style.listStyle = "none";
+	drop.style.whiteSpace = "normal";
 	for (let i = 0; i < ln.length; i++) {
 		const li = document.createElement("li");
 		li.classList.add("mapitem");
 		li.draggable = true;
-		li.id = window.jsonPlaylistData.GameObject.LevelPlaylist[0].LevelPath[i];
+		li.id = `${window.jsonPlaylistData.GameObject.LevelPlaylist[0].LevelPath[i]}--${padd(i, 10)}`;
 		li.innerText = ln[i];
 		li.addEventListener("dragstart", handleLi.drag);
 		ul.appendChild(li);
--- src/js/handleLi.js
+++ src/js/handleLi.js
@@ -13,10 +13,13 @@ const dropLi = (event) => {
 	console.log("Dropped item");
 	event.preventDefault();
 	const data = event.dataTransfer.getData("text");
-	event.target.appendChild(document.getElementById(data));
-	event.target.removeChild(document.getElementById(data));
+	const Li = document.getElementById(data);
+	const index = Array.from(document.getElementsByClassName("mapitem")).map(m => m.id).indexOf(data);
+	event.target.appendChild(Li);
+	// event.target.removeChild(Li);
+	Li.remove();
 
-	const index = window.jsonPlaylistData.GameObject.LevelPlaylist[0].LevelPath.indexOf(data);
+	// const index = document.getElementById().indexOf(data);
 	window.jsonPlaylistData.GameObject.LevelPlaylist[0].LevelName.splice(index, 1);
 	window.jsonPlaylistData.GameObject.LevelPlaylist[0].GameMode.splice(index, 1);
 	window.jsonPlaylistData.GameObject.LevelPlaylist[0].LevelPath.splice(index, 1);
